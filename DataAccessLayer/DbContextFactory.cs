14d495c037c350090e35f4eba4d8b2f048fa9265
Kerem Başçiftçi <kerem.bascifci@gazi.edu.tr>
2024-12-26 08:02:35 +0300

    migration -kerem

---
+++ DataAccessLayer/DbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO; // Directory.GetCurrentDirectory() için gerekli
+
+namespace DataAccessLayer
+{
+    public class DbContextFactory : IDesignTimeDbContextFactory<DataAccessLayer.DbContext> // Tam sınıf adı
+    {
+        public DataAccessLayer.DbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<DataAccessLayer.DbContext>();
+
+            // AppSettings.json veya başka bir kaynak üzerinden connection string'i alabilirsiniz
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("SqlConnection"));
+            return new DataAccessLayer.DbContext(optionsBuilder.Options, configuration); // Tam sınıf adı
+        }
+    }
+}
